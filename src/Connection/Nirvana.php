31b9292f8496f53c47f0a64528f3bac1965860ce
Kilian Bender <kilian+github@spinat.pizza>
2021-01-25 11:10:08 +0100

    Add a dummy connection class to disable loggin

---
+++ src/Connection/Nirvana.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MetricMetoy\Logger\Connection;
+
+use MetricMetoy\Logger\Connection as Connection;
+
+class Nirvana implements Connection
+{
+    public function send(string $message): void
+    {
+        // chill
+    }
+}
+
